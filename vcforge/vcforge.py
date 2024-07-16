0d97ddda7e42d54b92c299f78a99d8ad8ce4ab4d
matteobolner <matteo.bolner2@unibo.it>
2024-07-16 18:51:56 +0200

    Set up class with cyvcf2

--- vcforge/vcforge.py
+++ vcforge/vcforge.py
@@ -1,15 +1,31 @@
 import pandas as pd
-from vcforge.parsing import parse_input, build_var_ID
+from vcforge.parsing import *
 from typing import Dict
+from cyvcf2 import VCF, Writer
 
 
 class VCFClass:
-    def __init__(self, sample_id_column="sample", vcf=None, samples=None):
+    def __init__(
+        self,
+        sample_id_column="sample",
+        vcf_path=None,
+        sample_info=None,
+        add_info=False,
+        threads=1,
+    ):
+        self._vcf_path = vcf_path
         self._sample_id_column = sample_id_column
-        self.sample_info, self.vcf = self._setup_data(samples, vcf)
-        self.samples = list(self.sample_info.index)
-        self.var_info = self.vcf.drop(columns=self.samples)
-        self.var_data = self.vcf[self.samples]
+        self.sample_info, self.vcf = self._setup_data(sample_info, vcf_path)
+        self.vcf.set_threads(threads)
+        self.samples = self.vcf.samples
+        if add_info == True:
+            self.variants = get_vcf_metadata_and_info(VCF(vcf_path))
+        else:
+            self.variants = get_vcf_metadata(VCF(vcf_path))
+        self.var_ids = list(self.variants.index)
+        print(
+            f"VCF contains {len(self.variants)} variants over {len(self.samples)} samples"
+        )
 
     def _setup_data(self, input_sample_info, input_vcf) -> None:
         """
@@ -23,37 +39,29 @@ class VCFClass:
         Raises:
             ValueError: If the sample ID column is not found in the data.
         """
-        try:
-            # parse input data
-            parsed_samples = parse_input(input_sample_info)
-            parsed_vcf = parse_input(input_vcf)
-            sample_info = parsed_samples
-            if sample_info is None:
-                raise ValueError("Sample metadata is not properly initialized.")
-            if sample_info.index.name == self._sample_id_column:
-                sample_info = sample_info.reset_index()
-            if sample_info[self._sample_id_column].duplicated().any():
-                raise ValueError(
-                    "Warning: there are duplicate values in the chosen sample column."
-                )
-            sample_info[self._sample_id_column] = sample_info[
-                self._sample_id_column
-            ].astype(str)
-            sample_info.set_index(self._sample_id_column, inplace=True)
-            vcf = parsed_vcf
-            if vcf.index.name == "ID":
-                vcf.reset_index(inplace=True)
-            vcf.columns = [str(i) for i in vcf.columns]
-            if len(vcf["ID"].unique()) == 1:
-                vcf["ID"] = build_var_ID(vcf)
-            vcf.set_index("ID", inplace=True)
-            # Drop samples not in data
-            sample_info = sample_info[sample_info.index.isin(vcf.columns)]
-        except ValueError as ve:
-            raise ValueError(f"Error setting up data: {ve}")
+        parsed_samples = parse_table(input_sample_info)
+        sample_info = parsed_samples
+        if sample_info is None:
+            raise ValueError("Sample metadata is not properly initialized.")
+        if sample_info.index.name == self._sample_id_column:
+            sample_info = sample_info.reset_index()
+        if sample_info[self._sample_id_column].duplicated().any():
+            raise ValueError(
+                "Warning: there are duplicate values in the chosen sample column."
+            )
+        sample_info[self._sample_id_column] = sample_info[
+            self._sample_id_column
+        ].astype(str)
+        sample_info.set_index(self._sample_id_column, inplace=True)
+        vcf = VCF(input_vcf)
+        samples = [i for i in sample_info.index if i in vcf.samples]
+        sample_info = sample_info.loc[samples]
+        vcf.set_samples(samples)
         return sample_info, vcf
 
-    def split_by_sample_column(self, column: list) -> Dict[str, "VCF"]:
+    def split_by_sample_column(
+        self, column: list, add_info: bool = False
+    ) -> Dict[str, "VCF"]:
         """
         Split the dataset (data and sample metadata) in multiple independent VCF instances
         based on the values of one or more sample metadata columns.
@@ -72,49 +80,41 @@ class VCFClass:
             values of the sample metadata column and the values are the VCF instances
             containing the split data.
         """
-        split_data: Dict[str, VCF] = {}
+        split_data: Dict[str, VCFClass] = {}
         for name, group in self.sample_info.groupby(by=column):
-            var_data = self.vcf[list(group.index)]
-            vcf = pd.concat([self.var_info, var_data], axis=1)
+            print(name)
             tempclass = VCFClass(
-                sample_id_column=self._sample_id_column, vcf=vcf, samples=group
+                sample_id_column=self._sample_id_column,
+                vcf_path=self._vcf_path,
+                sample_info=group,
+                add_info=add_info,
             )
             split_data[name] = tempclass
         return split_data
 
+    def reset_vcf_iterator(self):
+        self.vcf = VCF(self._vcf_path)
+        self.vcf.set_samples(self.samples)
 
-a = VCFClass(
-    samples="/home/pelmo/work/workspace/PRSSV_candidate_genes/config/samples.tsv",
-    vcf="/home/pelmo/work/workspace/PRSSV_candidate_genes/data/variants/gatk/vep_annotation/VIM/snvs_biallelic.vcf",
-)
+    def format(self, format, allele):
+        vars_format = get_var_format_from_vcf(self.vcf, format, allele)
+        self.reset_vcf_iterator()
+        return vars_format
 
+    def subset_samples(self, samples):
+        samples = self.sample_info.loc[samples]
+        print(samples)
+        return VCFClass(
+            sample_id_column=self._sample_id_column,
+            sample_info=samples,
+            vcf_path=self._vcf_path,
+            add_info=False,
+        )
 
-
-
-a.var_info.columns
-
-
-t = a.split_by_sample_column("species")
-
-
-a.var_info
-
-
-var_info = df.drop(columns=samples["sample"].tolist())
-var_info = var_info.set_index("ID")
-var_info_first_cols = list(var_info.columns)
-var_info_first_cols.remove("INFO")
-
-var_info = var_info.apply(add_info_fields_to_row, axis=1)
-
-var_info = var_info[
-    var_info_first_cols + [i for i in var_info.columns if i not in var_info_first_cols]
-]
-var_info = var_info.drop(columns=["INFO"])
-
-df = df.set_index("ID")
-
-var_data = df[["FORMAT"] + samples["sample"].tolist()]
-
-
-a.vcf
+    def save_vcf(self, save_path):
+        w = Writer(save_path, self.vcf)
+        for v in self.vcf:
+            w.write_record(v)
+        w.close()
+        self.reset_vcf_iterator()
+        print(f"VCF saved to {save_path}")
